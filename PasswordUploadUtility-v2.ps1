5ad62ab0136bc079e118dbf5f81467d8be16768d
Joe Arida <37410578+Level99-Joe@users.noreply.github.com>
2018-03-15 10:03:58 -0500

    Add files via upload
    Updated PUUv2 and template to handle Oracle DB and Active Directory domain accounts. Changelog documents changes line by line. Also documented at the top of the script in the comments.

--- PasswordUploadUtility-v2.ps1
+++ PasswordUploadUtility-v2.ps1
@@ -11,21 +11,42 @@
 ################## WELCOME TO CYBERARK IMPACT 2017! #####################
 #
 # TODO:         Add Bulk Change Method
-#               Add Additional Properties for non-Windows accounts
+#               Add Additional Properties for non-Windows accounts -- added by Joe Arida
+#
+# CHANGELOG: changed to use 2-factor RADIUS instead of LDAP. Updated functions to support Oracle DB and AD Domain accounts.
+#    line 39: commented out as it was causing errors in my environment and I couldn't get it resolved. Can uncomment if desired for newer TLS.
+#    line 59-60: commented out LDAP password
+#    line 63-64: added 2-factor Radius auth
+#    line 67: added useRadiusAuthentication = 'True' to leverage 2-factor instead of AD auth
+#    line 108: added additional params to function
+#    line 118-125: sends different parameters based off the attributes that are populated in the spreadsheet
+#                    if both database and logondomain are populated in the same row then it will only send database due to order of if else statement. Could adjust if desired.
+#    line 128: added to convert bodyParams to appropriate JSON syntax
+#    line 131: added Write-Host $bodyParams to be used for debugging. Throws up the JSON on the screen to check syntax, uncomment when needed
+#    line 177: commented out apiPassword in favor of 2 factor auth
+#    line 207: moved the Replace(" ","") to remove spaces in platform ID from line 90 to this line, just so the syntax looked cleaner up above
+#    line 210-212: added additional parameters for AD/oracle accounts
+#    line 230: updated the if/$getResult count to be "-gt 0" otherwise it would never add an account for me. Since if it counts something that is the same username/address then the count would be 1
+#    line 235-246: added if else statement for different types of accounts
+#
+# BUG: If multiple account types (e.g., Oracle and AD) are in the same import file, then the additional properties (Port, LogonDomain, Database) can show up on accounts as artifacts.
+#        Example: if row two has an account with Port/Database populated, then row 3 might also have port and database get populated, even if it is blank. I am not sure why, maybe a caching issue?
+#        Workaround: just use separate import files for different types of accounts (1 file for Oracle, 1 for AD, 1 for local, etc)
+#
+# TODO: add parameter for "Limit Domain Access To"
 #
 #########################################################################
 
 ## Use TLS 1.2 instead of default 1.0
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 ## FUNCTIONS FIRST!
-function OpenFile-Dialog($initialDirectory)
-{
+function OpenFile-Dialog($initialDirectory) {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
     
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
     $OpenFileDialog.initialDirectory = $initialDirectory
-    $OpenFileDialog.filter = "CSV (*.csv)| *.csv"
+    $OpenFileDialog.filter = "CSV (*.csv) | *.csv"
     $OpenFileDialog.ShowDialog() | Out-Null
     $OpenFileDialog.filename
 }
@@ -33,13 +54,19 @@ function OpenFile-Dialog($initialDirectory)
 function PASREST-Logon {
 
     # Declaration
-    $webServicesLogon = "$Global:baseURL/PasswordVault/WebServices/auth/Cyberark/CyberArkAuthenticationService.svc/Logon"
+    $webServicesLogon = "$baseURL/PasswordVault/WebServices/auth/Cyberark/CyberArkAuthenticationService.svc/Logon"
 
-    # Authentication
-    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Global:apiPassword)
-    $apiPasswordPT = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    # Authentication - changed
+    ## use this for hard-coded LDAP or password authentication
+    #$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Global:apiPassword)
+    #$apiPasswordPT = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
     
-    $bodyParams = @{username = $Global:apiUsername; password = ($apiPasswordPT)} | ConvertTo-JSON
+    ##use this for 2-factor authentication
+    $password = Read-Host -Prompt Password -AsSecureString
+    $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+
+    #if using LDAP or password auth then change "useRadiusAuthentication to "False", uncomment lines 47 and 48 and 165, and comment out lines 51 an d 52
+    $bodyParams = @{username = "$apiUsername"; password = $password; useRadiusAuthentication = "true"; connectionNumber = "25"} | ConvertTo-JSON
 
     # Execution
     try {
@@ -54,9 +81,9 @@ function PASREST-Logon {
     }
 }
 
-function PASREST-GetAccount ([string]$Authorization,[string]$Address,[string]$Username, [string]$Safe="") {
+function PASREST-GetAccount ([string]$Authorization,[string]$Address,[string]$Username,[string]$Safe="") {
     # Declaration
-    $webServicesGetAccount = "$Global:baseURL/PasswordVault/WebServices/PIMServices.svc/Accounts"
+    $webServicesGetAccount = "$baseURL/PasswordVault/WebServices/PIMServices.svc/Accounts"
 
     # Authorization
     $headerParams = @{}
@@ -79,15 +106,31 @@ function PASREST-GetAccount ([string]$Authorization,[string]$Address,[string]$Us
     }
 }
 
-function PASREST-AddAccount ([string]$Authorization,[string]$ObjectName,[string]$Safe,[string]$PlatformID,[string]$Address,[string]$Username,[string]$Password,[boolean]$DisableAutoMgmt,[string]$DisableAutoMgmtReason) {
+#added additional parameters to function
+function PASREST-AddAccount ([string]$Authorization,[string]$ObjectName,[string]$Safe,[string]$PlatformID,[string]$Address,[string]$Username,[string]$Password,[string]$DisableAutoMgmt,[string]$DisableAutoMgmtReason,[string]$database,[string]$port,[string]$logonDomain) {
 
     # Declaration
-    $webServicesAddAccount = "$Global:baseURL/PasswordVault/WebServices/PIMServices.svc/Account"
+    $webServicesAddAccount = "$baseURL/PasswordVault/WebServices/PIMServices.svc/Account"
 
     # Authorization
     $headerParams = @{}
     $headerParams.Add("Authorization",$Authorization)
-    $bodyParams = @{account = @{safe = $Safe; platformID = $PlatformID.Replace(" ",""); address = $Address; accountName = $ObjectName; password = $Password; username = $Username; disableAutoMgmt = $DisableAutoMgmt; disableAutoMgmtReason = $DisableAutoMgmtReason}} | ConvertTo-JSON -Depth 2
+
+    #depending on what fields are populated in the row it will assume they are different types of accounts and send different body params
+    if ($database) {
+        $bodyParams = @{"account"=@{"safe"="$Safe";"platformID"="$PlatformID";"address"="$Address";"accountName"="$ObjectName";"password"="$Password";"username"="$Username";"disableAutoMgmt"="$DisableAutoMgmt";"disableAutoMgmtReason"="$DisableAutoMgmtReason";"properties"=@(@{Key="port";Value="$port"};@{Key="database";Value="$database"})}}
+    } elseif ($port) {
+        $bodyParams = @{"account"=@{"safe"="$Safe";"platformID"="$PlatformID";"address"="$Address";"accountName"="$ObjectName";"password"="$Password";"username"="$Username";"disableAutoMgmt"="$DisableAutoMgmt";"disableAutoMgmtReason"="$DisableAutoMgmtReason";"properties"=@(@{Key="port";Value="$port"};@{Key="database";Value="$database"})}}
+    } elseif ($logondomain) {
+        $bodyParams = @{"account"=@{"safe"="$Safe";"platformID"="$PlatformID";"address"="$Address";"accountName"="$ObjectName";"password"="$Password";"username"="$Username";"disableAutoMgmt"="$DisableAutoMgmt";"disableAutoMgmtReason"="$DisableAutoMgmtReason";"properties"=@(@{Key="logonDomain";Value="$logonDomain"})}}
+    } else {
+        $bodyParams = @{"account"=@{"safe"="$Safe";"platformID"="$PlatformID";"address"="$Address";"accountName"="$ObjectName";"password"="$Password";"username"="$Username";"disableAutoMgmt"="$DisableAutoMgmt";"disableAutoMgmtReason"="$DisableAutoMgmtReason"}}
+    }
+
+    $bodyParams = ConvertTo-JSON $bodyParams -Depth 4
+
+    #allows to see JSON being sent to server; used for debugging if methods are failing
+    #Write-host $bodyParams 
 
     # Execution
     try {
@@ -105,7 +148,7 @@ function PASREST-AddAccount ([string]$Authorization,[string]$ObjectName,[string]
 function PASREST-Logoff ([string]$Authorization) {
 
     # Declaration
-    $webServicesLogoff = "$Global:baseURL/PasswordVault/WebServices/auth/Cyberark/CyberArkAuthenticationService.svc/Logoff"
+    $webServicesLogoff = "$baseURL/PasswordVault/WebServices/auth/Cyberark/CyberArkAuthenticationService.svc/Logoff"
 
     # Authorization
     $headerParams = @{}
@@ -124,9 +167,6 @@ function PASREST-Logoff ([string]$Authorization) {
     }
 }
 
-## DISABLE SSL VERIFICATION (THIS IS FOR DEV ONLY!)
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
-
 ## SPLASH SCREEN
 Write-Host "Welcome to Password Upload Utility v2" -ForegroundColor "Yellow"
 Write-Host "=====================================" -ForegroundColor "Yellow"
@@ -135,8 +175,11 @@ Write-Host " "
 ## USER INPUT
 $Global:baseURL = Read-Host "Please enter your PVWA address (https://pvwa.cyberark.local)"
 $Global:apiUsername = Read-Host "Please enter your REST API Username (CyberArk/LDAP/RADIUS)"
-$Global:apiPassword = Read-Host "Please enter ${apiUsername}'s password" -AsSecureString
+#apiPassword only used for password auth, not radius
+#$Global:apiPassword = Read-Host "Please enter ${apiUsername}'s password" -AsSecureString
 $csvPath = OpenFile-Dialog($Env:CSIDL_DEFAULT_DOWNLOADS) 
+
+
 
 ## LOGON TO CYBERARK WEB SERVICES
 $sessionID = PASREST-Logon
@@ -158,35 +201,52 @@ foreach ($row in $csvRows) {
     # INCREMENT COUNTER
     $counter++
 
+    # added variables for oracle/AD account types
     # DEFINE VARIABLES FOR EACH VALUE
     $objectName             = $row.ObjectName
     $safe                   = $row.Safe
     $password               = $row.Password
-    $platformID             = $row.PlatformID
+    $platformID             = $row.PlatformID.Replace(" ","")
     $address                = $row.Address
     $username               = $row.Username
+    $database               = $row.Database
+    $port                   = $row.Port
+    $logonDomain            = $row.LogonDomain
 
     # If DisableAutoMgmt is yes or true, disable it.  Otherwise, ignore.
     if ($row.DisableAutoMgmt -eq "yes" -or $row.DisableAutoMgmt -eq "true") {
-        $disableAutoMgmt = $true
+        $disableAutoMgmt = "true"
     } else {
-        $disableAutoMgmt = $false
+        $disableAutoMgmt = "false"
     }
-    if ($disableAutoMgmt -eq $true) {
+    if ($disableAutoMgmt -eq "true") {
         $disableAutoMgmtReason = $row.DisableAutoMgmtReason
     } else {
         $disableAutoMgmtReason = ""
     }
 
     #CHECK IF ACCOUNT ALREADY EXISTS IN VAULT
-#    $getResult = PASREST-GetAccount -Authorization $sessionID -Address $address -Username $username -Safe $safe
-#    if ($getResult -ne $false) {
-#        # If results are returned matching the specific username and address combination, break to the next row.
-#        if([int]$getResult.Count -le 0) { Write-Host "[ERROR] Username ${username}@${address} already exists in the Vault." -ForegroundColor "Red"; continue }
-#    }
+    $getResult = PASREST-GetAccount -Authorization $sessionID -Address $address -Username $username -Safe $safe 
+    if ($getResult -ne $false) {
+        # If results are returned matching the specific username and address combination, break to the next row.
+        if([int]$getResult.Count -gt 0) { Write-Host "[ERROR] Username ${username}@${address} already exists in the Vault." -ForegroundColor "Red"; continue }
+    }
 
+    #if else statement has different parameters for different types of accounts
     # ADD ACCOUNT TO VAULT
-    $addResult = PASREST-AddAccount -Authorization $sessionID -ObjectName $objectName -Safe $safe -PlatformID $platformID -Address $address -Username $username -Password $password -DisableAutoMgmt $disableAutoMgmt -DisableAutoMgmtReason $disableAutoMgmtReason
+    if (-NOT([String]::IsNullOrEmpty($database))) {
+        $addResult = PASREST-AddAccount -Authorization $sessionID -ObjectName $objectName -Safe $safe -PlatformID $platformID -Address $address -Username $username -Password $password -DisableAutoMgmt $disableAutoMgmt -DisableAutoMgmtReason $disableAutoMgmtReason `
+            -database $database -port $port
+    } elseif (-NOT([String]::IsNullOrEmpty($port))) {
+        $addResult = PASREST-AddAccount -Authorization $sessionID -ObjectName $objectName -Safe $safe -PlatformID $platformID -Address $address -Username $username -Password $password -DisableAutoMgmt $disableAutoMgmt -DisableAutoMgmtReason $disableAutoMgmtReason `
+            -database $database -port $port
+    } elseif (-NOT([String]::IsNullOrEmpty($logonDomain))) {
+        $addResult = PASREST-AddAccount -Authorization $sessionID -ObjectName $objectName -Safe $safe -PlatformID $platformID -Address $address -Username $username -Password $password -DisableAutoMgmt $disableAutoMgmt -DisableAutoMgmtReason $disableAutoMgmtReason `
+            -logonDomain $logonDomain
+    } else {
+        $addResult = PASREST-AddAccount -Authorization $sessionID -ObjectName $objectName -Safe $safe -PlatformID $platformID -Address $address -Username $username -Password $password -DisableAutoMgmt $disableAutoMgmt -DisableAutoMgmtReason $disableAutoMgmtReason
+    }
+
     # If nothing is returned, there was an error and it will break to next row.
     if ($addResult -eq $false) { Write-Host "[ERROR] There was an error adding ${username}@${address} into the Vault." -ForegroundColor "Red"; continue }
     else { Write-Host "[INFO] [${counter}/${rowCount}] Added ${username}@${address} successfully." -ForegroundColor "DarkYellow" }
